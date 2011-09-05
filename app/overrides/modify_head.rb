bd879274d69037b1e3563d0787cb8d1fa700238b
Slava Mikerin <mikerin.slava@gmail.com>
2011-09-04 18:08:32 -0700

    added admin favicon

--- app/overrides/modify_head.rb
+++ app/overrides/modify_head.rb
@@ -4,3 +4,9 @@ Deface::Override.new(:virtual_path => 'layouts/spree_application',
                      :text => %q{<link rel="shortcut icon" href="/assets/favicon.ico" />})
 
 
+Deface::Override.new(:virtual_path => 'layouts/admin',
+                     :name => 'admin_add_favicon',
+                     :insert_bottom => "[data-hook='admin_inside_head']",
+                     :text => %q{<link rel="shortcut icon" href="/assets/favicon.ico" />})
+
+

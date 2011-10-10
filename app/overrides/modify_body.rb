693bc03f88f6da33c37f9f626b4c4958dc825554
Slava Mikerin <mikerin.slava@gmail.com>
2011-10-10 06:10:27 -0700

    added forgot_password_menu

--- app/overrides/modify_body.rb
+++ app/overrides/modify_body.rb
@@ -2,3 +2,8 @@ Deface::Override.new(:virtual_path => 'layouts/spree_application',
                      :name => 'add_signin_menu',
                      :insert_bottom => "[data-hook='body']",
                      :partial => 'shared/signin_menu')
+
+Deface::Override.new(:virtual_path => 'layouts/spree_application',
+                     :name => 'add_forgot_password_menu',
+                     :insert_bottom => "[data-hook='body']",
+                     :partial => 'shared/forgot_password_menu')

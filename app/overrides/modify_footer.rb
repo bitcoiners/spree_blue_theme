c05175620a1b580bfb43f261ddb0a0e46f8185bf
Slava Mikerin <mikerin.slava@gmail.com>
2011-11-24 15:05:41 -0800

    renamed footer edit

--- app/overrides/modify_footer.rb
+++ app/overrides/modify_footer.rb
@@ -5,3 +5,5 @@ Deface::Override.new(:virtual_path => 'layouts/spree_application',
 Deface::Override.new(:virtual_path => 'layouts/admin',
                      :name => 'remove_footer',
                      :remove => '#footer[data-hook]')
+
+#mail_to "me@domain.com", "Send Us an Email", :cc => "ccaddress@domain.com", :subject => "This is an example email"
